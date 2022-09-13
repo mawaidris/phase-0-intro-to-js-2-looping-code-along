ec2f2793de6273f577ba558045c695ae3801fbc7
mawa <mawahama05@gmail.com>
2022-09-13 15:35:34 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+const arr = ["Charlie", "Samip", "Ali"]
+
+function writeCards(arr){
+    for (i = 0; i < arr.length ; i++){
+        
+          console.log( `Thank you, ${arr[i]}, for the wonderful birthday gift!,`);
+       
+       } return writeCards
+   
+}
+writeCards(arr);
+
+
+function countDown (){
+    let countdown = 10;
+    while (countdown >= 0){
+        console.log(countdown--);
+    }
+}
+countDown();
